ab15a50cf90a58590ef56bd8fb5ff1c555b733ab
Jihoon Og <og@ualberta.ca>
2021-10-05 12:28:19 -0600

    Initial commit, all test pass need to document

--- httpclient.py
+++ httpclient.py
@@ -34,6 +34,11 @@ class HTTPResponse(object):
 
 class HTTPClient(object):
     #def get_host_port(self,url):
+    def parse_url(self, url):
+        result = urllib.parse.urlparse(url)
+        self.port = result.port if result.port else 80
+        self.path = result.path if result.path != "" else "/"
+        self.hostname = result.hostname
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -41,14 +46,31 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        code = data.split()[1]
+        return int(code)
 
-    def get_headers(self,data):
-        return None
+    def get_headers(self, data):
+        headers = data.split("\r\n\r\n")[0].split("\r\n")[1:]
+        header_info = {}
+        for header in headers:
+            field_name = header.split(":")[0]
+            field_value = header.split(":")[1]
+            header_info[field_name] = field_value
+        return header_info
 
     def get_body(self, data):
-        return None
+        body = data.split("\r\n\r\n")[1]
+        return body
     
+    # This will generate the body for a POST form in a dict
+    def generate_form(self, entity={}):
+        body = ""
+        for key, value in entity.items():
+            body += "{key}={value}&".format(key=key, value=value)
+        # Remove the extra &
+        if body != "":
+            body = body[:-1]
+        return body
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
         
@@ -68,13 +90,51 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        self.parse_url(url)
+        self.connect(self.hostname, self.port)
+        # Create the status line
+        body = "GET {path} HTTP/1.1\r\n".format(path=self.path)
+        # Set the headers
+        body += "Host: {host}:{port}\r\n".format(host=self.hostname, port=self.port)
+        body += "Connection: close\r\n"
+        body += "Accept: */*\r\n"
+        body += "\r\n"
+        # End of headers
+
+        # Send the requests
+        self.sendall(body)
+        response = self.recvall(self.socket)
+        code = self.get_code(response)
+        body = self.get_body(response)
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        self.parse_url(url)
+        self.connect(self.hostname, self.port)
+
+        if args != None:
+            form_body = self.generate_form(args)
+        else:
+            form_body = ""
+        # Create the status line
+        body = "POST {path} HTTP/1.1\r\n".format(path=self.path)
+        # Set the headers
+        body += "Host: {host}:{port}\r\n".format(host=self.hostname, port=self.port)
+        body += "Connection: close\r\n"
+        if args != None:
+            body += "Content-Type: application/x-www-form-urlencoded\r\n"
+            body += "Content-Length: {}\r\n".format(len(form_body))
+        else:
+            body += "Content-Length: {}\r\n".format(0)
+        body += "\r\n"
+        # End of Headers
+        body += form_body
+        
+        # send the request
+        self.sendall(body)
+        response = self.recvall(self.socket)
+        code = self.get_code(response)
+        body = self.get_body(response)
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
